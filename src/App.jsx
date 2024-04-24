985b3581eff1ac9440dac2f3c1ab3027b2b9fe5e
AmorFatiBA <amor.fati291708@gmail.com>
2024-04-24 20:54:05 +0200

    pagina's en routes gemaakt

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Profile from "./pages/Profile";
+import OverMij from "./pages/OverMij";
+import Home from "./pages/Home";
 
 export default function App() {
-  return <div className="text-red-700">App</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/over-mij" element={<OverMij />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
