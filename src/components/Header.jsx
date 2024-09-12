75c8667011671c3be217cffcecc397640250bdde
AmorFatiBA <amor.fati291708@gmail.com>
2024-09-12 17:58:55 +0200

    Header component bijgewerkt

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,39 @@
 import React from "react";
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <header className="bg-orange-300 ">
-      <div>
-        <h1 className="font-bold text-sm sm:text-xl">
-          <span className="text-gray-100 ">FES</span>
-          <span className="text-blue-950">Makelaar</span>
+    <header className="bg-white h-40 border-b-2">
+      <div className="bg-white h-12 border-b-2"></div>
+      <div className="flex justify-between items-center h-28 mx-auto p-3 max-w-6xl">
+        <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+          <span className="text-orange-400 ">bdl</span>
+          <span className="text-orange-600">Makelaar</span>
         </h1>
+        <form className="bg-stone-100  rounded-lg p-0 border  flex items-center">
+          <input
+            className="bg-transparent px-4 py-2  outline-amber-600  w-24 sm:w-64"
+            type="text"
+            placeholder="Zoeken..."
+          />
+          <FaSearch className="text-orange-600 mx-4" />
+        </form>
+        <ul className="flex gap-4 items-center">
+          <Link to="/">
+            <li className="hidden sm:inline text-stone-600 hover:underline">
+              Home
+            </li>
+          </Link>
+          <Link to="/over-mij">
+            <li className="hidden sm:inline text-stone-600 hover:underline">
+              Over mij
+            </li>
+          </Link>
+          <Link to="/sign-in">
+            <li className="text-stone-600 hover:underline">Sign In</li>
+          </Link>
+        </ul>
       </div>
     </header>
   );
